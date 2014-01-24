e763c72952adb5abd441b3a1a3421219179e569d
Insomniak92 <alexiparakian@hotmail.fr>
2014-01-24 17:24:07 +0100

    Création de l'app RoR

---
+++ app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = '027c67b6e4cb0aae3c37516e0003943979a8958048bc4c7d16fd665b152acce3e973fb15915983d1b6ab48f83a93d653e33d4d2dc8b14ee17df37feed7da603e'
