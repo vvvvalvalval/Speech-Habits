76e223a9c979e8577a977d4426668aa43a4bac7b
Valentin Waeselynck <val.vvalval@gmail.com>
2014-02-03 02:32:24 +0100

    Implemented a mock user interface with 2 views.

--- speech-habits-client/www/js/app.js
+++ speech-habits-client/www/js/app.js
@@ -1,16 +1,21 @@
-'use strict';
+"use strict";
 
-
-// Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+/**
+ * Here we declare the Speech Habits app
+ */
+angular.module('speechHabitsApp', 
+['ngRoute','speechHabitsControllers'])
+.config(['$routeProvider', function($routeProvider) {
+	/*
+	 * This part declares the routing configuration that will enable us to have multiple views
+	 */
+	$routeProvider.when('/teachersList', {
+		templateUrl : 'partials/teachers_list.html',
+		controller : 'TeachersListController'
+	}).when('/rooms/:teacherId',{
+		templateUrl : 'partials/room.html',
+		controller : 'roomController'
+	}).otherwise({
+		redirectTo : '/teachersList'
+	});
 }]);
--- speech-habits-client/www/js/controllers.js
+++ speech-habits-client/www/js/controllers.js
@@ -1,11 +1,124 @@
-'use strict';
+"use strict";
 
-/* Controllers */
+var speechHabitsControllers = angular.module('speechHabitsControllers', []);
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+/*
+ * Here's the controller displaying the list of available teachers
+ */
+speechHabitsControllers.controller('TeachersListController', ['$scope',
+function($scope) {
 
-  }])
-  .controller('MyCtrl2', [function() {
+	$scope.message="Coucou!";
 
-  }]);
+	/**
+	 *Factory function that returns a newly created teacher with the specified name.
+	 *The returned teacher has 'id' and 'name' properties.
+	 * @param {String} name
+	 */
+	var makeNewTeacher = ( function() {
+
+			//private incrementor to generate IDs
+			var nextId = 0;
+
+			function makeNewId() {
+				var result = nextId;
+				nextId += 1;
+				return result;
+			};
+
+			return function(name) {
+				var currentId = makeNewId();
+
+				return {
+					'id' : currentId,
+					'name' : name
+				};
+			};
+		}());
+
+	/**
+	 * The list of available teachers.
+	 */
+	$scope.teachersList = [
+		makeNewTeacher("Philippe Ginier-Gillet"),
+		makeNewTeacher("Bruno Martinaud"),
+		makeNewTeacher("Chuck Norris")
+	];
+}]);
+
+/**
+ * Here we declare the controller for a single room.
+ */
+speechHabitsControllers.controller('roomController', ['$scope', '$routeParams',
+function($scope, $routeParams) {
+
+	//retrieving the ID of the current teacher from the route parameters
+	var currentTeacherId = $routeParams['teacherId'];
+
+	/**
+	 * 
+	 * @param {int} teacherId : the ID of the teacher whose expressions are to be fetched.
+	 */
+	function obtainExpressions(teacherId){
+		//current implementation is a mock : always returns the same expressions, without heeding the teacher id
+		return [
+			newExpression("Basically"),
+			newExpression("By the way"),
+			newExpression("Somewhere, somehow"),
+			newExpression("At this point in time"),
+			newExpression("Having said that")
+		];
+	};
+
+	function requestIncrement(exprId, incrementCallback){
+		//Mmmmh... I don't know... do I increment you?
+		// Ok, I'll increment you
+		incrementCallback();
+	};
+
+	/**
+	 * Factory function for creating new expressions.
+	 * The created expression exposes methods for obtaining the current count, and asking to increment it.
+	 * @param {String} exprText the text of the expression to create
+	 */
+	var newExpression = ( function() {
+			//private incrementor to generate IDs
+			var nextId = 0;
+
+			function makeNewId() {
+				var result = nextId;
+				nextId += 1;
+				return result;
+			};
+			
+			return function(exprText){
+				// hidden variable for the counter
+				var myCounter = 0;
+				var myId = makeNewId();
+				
+				var incrementMe = function(){
+					myCounter += 1;
+				};
+				
+				var getCount = function(){
+					return myCounter;
+				};
+				
+				var askIncrement = function(){
+					requestIncrement(myId, incrementMe);
+				};
+				
+				return {
+					'id': myId,
+					'text': exprText,
+					'getCount': getCount,
+					'askIncrement': askIncrement	
+				};
+			};
+			
+		}());
+	
+	//exposing the expressions of the teacher in scope
+	$scope.expressions = obtainExpressions(currentTeacherId);
+		
+}]);
